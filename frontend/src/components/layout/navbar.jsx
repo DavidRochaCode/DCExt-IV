10d618dd5f4836a6ed0797fc5c198297675e2f99
David Rocha <manoeudavi3@gmail.com>
2023-04-23 17:04:19 -0300

    mudar a tag da logo

--- frontend/src/components/layout/navbar.jsx
+++ frontend/src/components/layout/navbar.jsx
@@ -7,7 +7,10 @@ function Navbar() {
     <div>
       <div className="navbar">
         <Link to="/">
-          <img src="https://drive.google.com/file/d/1EzC_bMDIXd5vH2_3ncbdfqluXv_ZyTDZ/view?usp=share_link" alt="Logo da navbar, está escrito DCExt-v" width="100"/>
+        <img
+        src="https://drive.google.com/file/d/1EzC_bMDIXd5vH2_3ncbdfqluXv_ZyTDZ/view?usp=sharing"
+        alt="dceIcon"
+      />
         </Link>
         <Link className='menu' to="/publicar">
           Publicar
